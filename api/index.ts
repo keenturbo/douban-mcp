b50b77a8f2efd08cce2242c05fc8a684fea4dea8
keenturbo <turbokeem@gmail.com>
2025-11-13 14:28:14 +0800

    更新 index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
-// 导入构建后的 Express 应用
-import app from '../dist/index.js';
-
-// 导出供 Vercel 使用
-export default app;
+   // 导入 Express 应用源文件
+   import app from '../src/index';
+   
+   // 导出供 Vercel 使用
+   export default app;
